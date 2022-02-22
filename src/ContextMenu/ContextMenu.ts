a3bd7c5955e22515376b8c015089c27752bae60a
Christian Dornetto <christian@crm-dynamics.co.uk>
2022-02-22 10:11:18 +0000

    Changed configuration structure
    moved call back to be menu based instead of option based

--- src/ContextMenu/ContextMenu.ts
+++ src/ContextMenu/ContextMenu.ts
@@ -1,8 +1,14 @@
 export class ContextMenu {
-  constructor(menuElement: HTMLElement, configuration: IContextMenuConfiguration, scope: HTMLElement, ...args: any[]) {
+  constructor(menuElement: HTMLElement, configuration: IContextMenuConfiguration, target: HTMLElement, args: object) {
     menuElement.classList.add(configuration.css.contextMenuClass);
-    scope.oncontextmenu = null;
-    scope.addEventListener('contextmenu', e => {
+    menuElement.oncontextmenu = e => e.preventDefault();
+    target.oncontextmenu = null;
+    target.addEventListener('contextmenu', e => {
+      const type = (e.target as HTMLElement).tagName;
+      if (type === 'INPUT' || type === 'SELECT') {
+        return;
+      }
+
       e.stopPropagation();
       e.preventDefault();
       menuElement.innerHTML = '';
@@ -10,20 +16,30 @@ export class ContextMenu {
       configuration.data.options?.forEach(o => {
         const item = menuElement.appendChild(document.createElement('li'));
         const option = item.appendChild(document.createElement('button'));
+        option.classList.add(configuration.css.menuItemClass);
+
         if (o.iconClassList) {
           const icon = option.appendChild(document.createElement('i'));
-          icon.classList.add('item-icon', ...o.iconClassList);
+          icon.classList.add(...o.iconClassList);
         }
-        option.classList.add(configuration.css.contextMenuItemClass);
-        const label = option.appendChild(document.createElement('span'));
+
+        const content = option.appendChild(document.createElement('div'));
+        content.classList.add(configuration.css.menuItemContentClass);
+
+        const label = content.appendChild(document.createElement('span'));
         label.textContent = o.label;
-        if (o.callback) {
-          option.addEventListener('click', e => o.callback?.call(this, args));
+
+        if (o.hint) {
+          const hint = content.appendChild(document.createElement('span'));
+          hint.classList.add(configuration.css.menuItemHintClass);
+          hint.textContent = o.hint;
         }
+
+        option.addEventListener('click', e => configuration.callback.call(this, { key: o.key, ...args }));
         option.addEventListener('click', () => menuElement.classList.remove(configuration.css.activeClass));
       });
 
-      const { normalisedX, normalisedY } = NormalisePosition(e.x, e.y, menuElement, scope);
+      const { normalisedX, normalisedY } = NormalisePosition(e.x, e.y, menuElement);
       menuElement.style.left = `${normalisedX}px`;
       menuElement.style.top = `${normalisedY}px`;
       menuElement.classList.remove(configuration.css.activeClass);
@@ -33,7 +49,7 @@ export class ContextMenu {
       });
     });
 
-    scope.addEventListener('click', e => {
+    document.body.addEventListener('click', e => {
       if ((e.target as HTMLElement).offsetParent != menuElement) {
         e.stopPropagation();
         menuElement.classList.remove(configuration.css.activeClass);
@@ -68,12 +84,15 @@ function NormalisePosition(x: number, y: number, menuElement: HTMLElement, scope
 interface IContextMenuConfiguration {
   data: IContextMenuData;
   css: IContextMenuStyle;
+  callback: Function;
 }
 
 interface IContextMenuStyle {
   contextMenuClass: string;
   activeClass: string;
-  contextMenuItemClass: string;
+  menuItemClass: string;
+  menuItemContentClass: string;
+  menuItemHintClass: string;
 }
 
 interface IContextMenuData {
@@ -81,7 +100,9 @@ interface IContextMenuData {
 }
 
 interface IContextMenuOption {
+  key: string;
   label: string;
+  hint?: string;
   iconClassList?: string[];
   callback?: Function;
 }
